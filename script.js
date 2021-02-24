37ddd2f9476b3531292e78dfa65f7fe170372fa6
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-02-24 11:54:52 +0600

    done with sticky nav

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-  
+  $('.section-features').waypoint(function(direction){
+      if(direction == 'down'){
+          $('nav').addClass('sticky');
+      } else{
+          $('nav').removeClass('sticky');
+      }
+
+  }, {
+      offset: '90%;'
+  });
 
 
 
