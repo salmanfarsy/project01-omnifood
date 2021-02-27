52a652fbb596228a087737c4ada4d5099c459325
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-02-27 11:31:57 +0600

    nav links done

--- script.js
+++ script.js
@@ -9,8 +9,27 @@ $(document).ready(function(){
   }, {
       offset: '90%;'
   });
+$('.btn-full').click(function(){
+   $('html, body').animate({scrollTop:$('.plan').offset().top}, 1000); 
+})
 
+$('.btn-ghost').click(function(){
+    $('html, body').animate({scrollTop:$('.section-features').offset().top}, 1000); 
+ })
 
-
+ $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 
 });
